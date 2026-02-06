c448e29f21c22c6060f307b97567c23c9e57fc30
Netofu <98113246+Netofu@users.noreply.github.com>
2026-02-06 15:44:01 -0300

    Create category.routes.ts

---
+++ src/routes/category.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+import { CategoryController } from '../controllers/category.controller';
+import { validate } from '../middlewares/validation.middleware';
+import { pagination } from '../middlewares/pagination.middleware';
+import {
+  createCategorySchema,
+  updateCategorySchema,
+  categoryQuerySchema
+} from '../validations/category.validation';
+
+const router = Router();
+
+// Todas as rotas usam paginação
+router.use(pagination);
+
+// CRUD de Categorias
+router.post(
+  '/',
+  validate(createCategorySchema),
+  CategoryController.createCategory
+);
+
+router.get(
+  '/',
+  CategoryController.getCategories
+);
+
+router.get(
+  '/:id',
+  CategoryController.getCategory
+);
+
+router.put(
+  '/:id',
+  validate(updateCategorySchema),
+  CategoryController.updateCategory
+);
+
+router.delete(
+  '/:id',
+  CategoryController.deleteCategory
+);
+
+export default router;
